d3caddfffc5bf2cf90c406228eab293dad472757
Shea Belsky <shb95@cornell.edu>
2017-04-12 18:27:53 -0400

    MongoDB Prep

--- modules/db.js
+++ modules/db.js
@@ -1,14 +1,6 @@
 var mongoDB     = require('mongodb').MongoClient;
 
-var connection_string = 'mongodb://127.0.0.1:27017/nodejs';
-
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-  connection_string = 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-  process.env.OPENSHIFT_APP_NAME;
-}
+var connection_string = process.env.MONGODB_URI;
 
 function connect(callback){
   mongoDB.connect(connection_string, function(err, db) {
