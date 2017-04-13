24a3b9410f1c75397adb578831b28e68d48d7cbf
Shea Belsky <sheabelsky@gmail.com>
2017-04-12 22:46:20 -0400

    Chuck

--- custom_commands/fun-commands.js
+++ custom_commands/fun-commands.js
@@ -1,4 +1,4 @@
-var cmds = [cmdChuckNorris, cmdFactSphere, cmdFlipCoin, cmdRollDice, cmdFunnyFace, cmdGangly, cmdRNG, cmdSavvas, cmdWarrenHall, cmdWater];
+var cmds = [cmdChuckNorris, cmdFactSphere, cmdFlipCoin, cmdKnockKnockJokes, cmdRollDice, cmdFunnyFace, cmdGangly, cmdRNG, cmdSavvas, cmdWarrenHall, cmdWater];
 
 exports.checkCommands = function(dataHash, callback) {
   for (cmd in cmds) {
@@ -24,55 +24,13 @@ exports.getCmdListDescription = function () {
   return cmdArr;
 }
 
-function cmdChuckNorris(request) {
-  var chuckNorris = [
-      "Chuck Norris and Superman once fought each other on a bet. The loser had to start wearing his underwear on the outside of his pants.",
-      "When Alexander Graham Bell invented the telephone he had 3 missed calls from Chuck Norris.",
-      "Chuck Norris doesn't flush toilets, he scares the shit out of them.",
-      "There used to be a street named after Chuck Norris, but it was changed because no one crosses Chuck Norris and lives.",
-      "Chuck Norris has a grizzly bear carpet in his room. The bear isn't dead it is just afraid to move.",
-      "Chuck Norris has already been to Mars; that's why there are no signs of life.",
-      "Fear of spiders is arachnophobia, fear of tight spaces is claustrophobia, fear of Chuck Norris is called logic.",
-      "Chuck Norris doesn't call the wrong number. You answer the wrong phone.",
-      "Some magicians can walk on water, Chuck Norris can swim through land.",
-      "Chuck Norris once urinated in a semi truck's gas tank as a joke....that truck is now known as Optimus Prime.",
-      "Chuck Norris can cut through a hot knife with butter.",
-      "Chuck Norris counted to infinity - twice.",
-      "Chuck Norris is the reason Waldo is hiding.",
-      "Death once had a near-Chuck Norris experience.",
-      "When the Boogeyman goes to sleep every night, he checks his closet for Chuck Norris.",
-      "Chuck Norris can slam a revolving door.",
-      "Chuck Norris once kicked a horse in the chin. Its descendents are known today as Giraffes.",
-      "Chuck Norris will never have a heart attack. His heart isn't foolish enough to attack him.",
-      "Chuck Norris once got bit by a rattle snake. After three days of pain and agony, the rattle snake died.",
-      "Chuck Norris can win a game of Connect Four in three moves.",
-      "Chuck Norris can light a fire by rubbing two ice cubes together.",
-      "When Chuck Norris does a pushup, he isn't lifting himself up, he's pushing the Earth down.",
-      "All species are endangered. There’s just a long list of animals Chuck Norris allows to live.",
-      "Chuck Norris doesn’t wear a watch. He decides what time it is.",
-      "Chuck Norris doesn't read books. He stares them down until he gets the information he wants.",
-      "Chuck Norris made a Happy Meal cry.",
-      "Outer space exists because it's afraid to be on the same planet with Chuck Norris.",
-      "When Chuck Norris swims across the Pacific, sharks hear the theme from Jaws.",
-      "If you spell Chuck Norris in Scrabble, you win. Forever.",
-      "Chuck Norris has the only hands that can beat a Royal Flush.",
-      "Chuck Norris destroyed the periodic table because Chuck Norris only recognizes the element of surprise.",
-      "Chuck Norris never loses weight. He knows exactly where it is.",
-      "Chuck Norris does not sleep. He waits.",
-      "Chuck Norris got a home run in bowling.",
-      "Some people wear Superman pajamas. Superman wears Chuck Norris pajamas.",
-      "Chuck Norris can install a program without accepting the terms & conditions.",
-      "The Chuck Norris-themed toilet prototype failed. It wouldn't take shit from anyone.",
-      "Chuck Norris doesn't breathe, he holds air hostage.",
-      "If Chuck Norris pokes you on Facebook, you won't live.",
-      "When cops pull Chuck Norris over, they try to talk their way out of it.",
-      "Don't google Chuck Norris. He will find you.",
-      "Chuck Norris died 20 years ago, Death just hasn't built up the courage to tell him yet."
-    ];
+function cmdChuckNorris(request, cb) {
     var regex = /^\/chucknorris$/i;
-
     if (regex.test(request.text)) {
-      return chuckNorris[Math.floor(Math.random()*chuckNorris.length)];
+      request("http://api.icndb.com/jokes/random", function (error, response, body) {
+          var responseJSON = JSON.parse(response);
+          return cb(responseJSON.value.joke);
+      });
     }
     else {
       return false;
@@ -216,6 +174,15 @@ function cmdFunnyFace(request) {
     }
 }
 
+function cmdKnockKnockJokes(request) {
+    var regex = /^\/knockknock$/i;
+    if (regex.test(request.text)) {
+        return require('knock-knock-jokes')();
+    } else {
+        return false;
+    }
+}
+
 function cmdRollDice(request){
   var regex = /^\/roll (\d+)d(\d+)/i;
 
@@ -256,12 +223,10 @@ function cmdGangly(request) {
 
 function cmdRNG(request) {
     var regex = /^\/rng (\d+)/i;
-    console.log(request.text);
     if (regex.test(request.text)) {
-        console.log("yas");
       var val = regex.exec(request.text);
       var random = require('random-js');
-      return random.integer(1, parseInt(val[1]))(random.engines.nativeMath) + "";
+      return "Your random number between 1 and " + val[1] + " is " + random.integer(1, parseInt(val[1]))(random.engines.nativeMath);
     }
     else {
         return false;
